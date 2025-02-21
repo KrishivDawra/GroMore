9bcb95c7572011c7610de35540b226adf164e809
KrishivDawra <krishivdawra@gmail.com>
2025-02-21 15:31:29 +0530

    header done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>
+    <div className='h-screen bg-[url()] '>
       
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <div className='flex justify-between '>
-
-      <div className='flex '>
-        <img className='h-[40px] w-[40px]' src=".\src\assets\logo.jpg" alt="" />
-        <h1 className='px-2 py-2 size- font-serif text-red-300'>EggNest</h1>
+    <div className='flex justify-between items-center  '>
+      <div>
+        <span className='font-serif text-3xl font-bold text-red-500'>Gro</span>
+        <span className='font-serif text-3xl font-bold text-white'>More</span>
       </div>
-            
-      <ul className=' flex gap-5 px-6 py-3 border-2 bg-gray-50 rounded-2xl shadow-md shadow-green-300 '>
-        <li className=' hover:text-green-600'><Link to={'/'}>Home</Link></li>
-        <li className=' hover:text-green-600'><Link to={'/about'}>About Us</Link></li>
-        <li className=' hover:text-green-600'><Link to={'/services'}>Services</Link></li>
-        <li className=' hover:text-green-600' ><Link to={'/product'}></Link></li>
-      </ul>
       
-      <div className='gap-10'>
-        <a href="#" className='px-2'>Sign In</a>
-        <button className='bg-green-500 hover:bg-green-600 px-5 py-2 rounded-full'>Join Us</button>
+
+      <div className='flex gap-8'>
+        <h1 className='font-serif text-xl text-white cursor-pointer'><Link to={'/'}>Home</Link></h1>
+        <h1 className='font-serif text-xl text-white'><Link>TradeView</Link></h1>
+        <h1 className='font-serif text-xl text-white'><Link>News</Link></h1>
+        <h1 className='font-serif text-xl text-white'><Link>Courses</Link></h1>
+        <h1 className='font-serif text-xl text-white'><Link>About</Link></h1>
       </div>
+
+
     </div>
 
 
